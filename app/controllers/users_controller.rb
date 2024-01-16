a124d81d35aeb0ac45f8da00545cfca73f11b5cf
liubo35 <boliu.org@gmail.com>
2024-01-16 23:15:43 +0800

    add welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.with(user: @user).welcome_email.deliver_later
       session[:user_id]
       redirect_to app_path, notice: "Signed up successfully"
     else
--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Marklog
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_mailer.default_url_options = { host: "localhost:3000" }
   end
 end
